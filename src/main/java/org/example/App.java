b18d913503986f1bdf1f2e03f6a77d99b61ae49f
Prakriti Singh <prsing@griddynamics.com>
2026-02-23 12:08:14 +0530

    added laptop

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,3 +12,7 @@ public class App
         obj.code();
     }
 }
+//We created a constructor which runs, is it happening bcs of line no. 11 or 12
+//On line no 9, we are just not creating a container, but also specifying, that there's a file, spring.xml, go to the file
+//and create all the obj's that are mentioned with the bean tag
+//on line 11 and 12, we are not creating an obj, but just calling the object, by saying get bean.
--- src/main/java/org/example/Class1.java
+++ src/main/java/org/example/Class1.java
@@ -1,6 +1,11 @@
 package org.example;
 
 public class Class1 {
+
+    public Class1()
+    {
+        System.out.println("Object Created");
+    }
     public void code()
     {
         System.out.println("My first Spring Project");
